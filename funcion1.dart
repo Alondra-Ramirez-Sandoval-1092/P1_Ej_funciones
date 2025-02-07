4338662e37aba5413a1c9658a0e56bfe8414094f
Alondra-Ramirez-Sandoval-1092 <164534038+Alondra-Ramirez-Sandoval-1092@users.noreply.github.com>
2025-02-07 16:48:37 +0000

    Mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+// funcion que no devuelva valor 
+// funcion saludar 
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('Alondra Ramirez')
+}
+
+void main(){
+  // Alondra Ramirez mat: 22308051281092
+  // Llamar a la funcion saludar 
+  print('Alondra rAmirez mat: 22308051281092');
+  print('Llamando a la funcion saludar');
+  saludar();
+  print('Llamando a la funcion escribeNombre');
+  escribeNombre();
+
+}
