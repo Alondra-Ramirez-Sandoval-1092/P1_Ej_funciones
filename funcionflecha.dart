bd8e3e19a625c2aa5e62f574ab12a1a994176010
Alondra-Ramirez-Sandoval-1092 <164534038+Alondra-Ramirez-Sandoval-1092@users.noreply.github.com>
2025-02-10 18:32:24 +0000

    publique funcion flecha

---
+++ funcionflecha.dart
@@ -0,0 +1,26 @@
+int multiplicacion(int a, int b){
+  return a * b;
+}
+
+int flechamultiplicacion(int a, int b) => a * b;
+//returnType functionName(parameters...) => expression;
+
+double division(double a, double b){
+  return a / b;
+}
+
+double flechadivision(double a, double b) => a / b;
+
+// funcion main
+void main(){
+  print('Alondra Ramirez SAndoval mat: 22308051281092');
+  print('Llamando a la funcion multiplicacion');
+  print(multiplicacion(3, 5));
+  print("Llamando a la funcion flechamultiplicacion");
+  print(flechamultiplicacion(3, 5));
+
+  print('Llamando a la funcion division');
+  print(division(10, 3));
+  print("Llamando a la funcion flechadivision");
+  print(flechadivision(10, 3));
+}
