3470b7b6b24856e3702eaac368b2055d619f8e48
Alondra-Ramirez-Sandoval-1092 <164534038+Alondra-Ramirez-Sandoval-1092@users.noreply.github.com>
2025-02-10 18:56:50 +0000

    funcion parametros

---
+++ funcionparam.dart
@@ -0,0 +1,18 @@
+// Primera función: Saluda a una persona
+void saludar(String nombre, String mensaje) {
+  print("$mensaje, $nombre!");
+}
+
+// Segunda función: Suma dos números
+void edad(int fecha, int edad) {
+  print("naciste en $fecha, tienes $edad años");
+}
+
+void main() {
+  print('Alondra Ramirez Sandoval mat: 22308051281092');
+  // Llamada a la primera función
+  saludar("Seokjin", "Holaaa");
+
+  // Llamada a la segunda función
+  edad(1992, 33);
+}
--- recursos/funcionsuma.md
+++ recursos/funcionsuma.md
@@ -3,3 +3,6 @@ Ejemplo de salida
 
 ejemplo de salida funcion normal + funcion flecha
 ![alt text](image-3.png)
+
+ejemplo de salida funcion 2 parametros 
+![alt text](image.png)
