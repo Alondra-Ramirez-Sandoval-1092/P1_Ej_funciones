25c8eaa27de82321abd6fcf95337040fe90d144f
Alondra-Ramirez-Sandoval-1092 <164534038+Alondra-Ramirez-Sandoval-1092@users.noreply.github.com>
2025-02-10 18:05:13 +0000

    publique funcion suma

---
+++ funcion2.dart
@@ -0,0 +1,14 @@
+void suma(int a, int b){
+  print('La suma es $a + $b: ${a + b}');
+}
+
+void resta(int a, int b){
+  print('La resta es $a - $b: ${a - b}');
+}
+
+void main(){
+  print('Alondra Ramirez SAndoval mat: 22308051281092');
+  print('Llamando a la funcion suma y resta');
+  suma(10, 5);
+  resta(10, 5);
+}
